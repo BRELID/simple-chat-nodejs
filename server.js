6823843be4118bca131ca4e4345db468b7d068ee
BRELID <brahim.relid@gmail.com>
2019-07-24 16:53:17 +0200

    add few comments

--- server.js
+++ server.js
@@ -6,20 +6,34 @@ var fs = require("fs");
 
 server.listen(8080)
 
+
+
 app.get('/', (req, res) => {
 
     res.render('index.ejs');
 });
 
+// Open socket with each new connection
+// Ouvre un canal (socket) pour chaque nouvelle connection
 io.on("connection", socket => {
 
+    // Listen event "pseudo" from the client
+    // Ecoute l'évènement "pseudo" provenant du client
     socket.on("pseudo", pseudo => {
+
+        // Get the current time
+        // Récupère le temps courant
         let now_time = getTime();
 
         socket.pseudo = ent.encode(pseudo);
         console.log("pseudo: " + socket.pseudo);
+
+        // Write in the log, the new client
+        // Ecrit dans les logs l'arrivé d'un nouveau client
         writeLog(now_time + " - " + "new chatter :" + socket.pseudo)
 
+        // Emit a new event with data for to show in the client side
+        // Emet un nouvelle évènement avec des données pour l'afficher du côté client
         io.emit("new_chatter", {
             pseudo: socket.pseudo,
             time: now_time
@@ -57,8 +71,13 @@ io.on("connection", socket => {
 /**
  * 
  * Functions
+ * 
  **/
 
+ /**
+  * Return a string of the current time
+  * Retoune une chaine de caractère avec le temps courant
+  */
 function getTime() {
     let date = new Date();
     let h = (date.getHours() < 10 ? '0' : '') + date.getHours();
@@ -68,6 +87,11 @@ function getTime() {
     return h + ':' + m + ':' + s;
 }
 
+/**
+ * Add content in the file log : chat-log.txt
+ * Rajoute du contenu dans le fichier log : chat-log.txt
+ * @param {string} content 
+ */
 function writeLog(content) {
 
     fs.appendFile("chat-log.txt", content+"\r\n", (err) => {
