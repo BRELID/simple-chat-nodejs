85d5b5c05b01530bc9bd8a6a5637c03046195479
BRELID <brahim.relid@gmail.com>
2019-07-24 14:57:22 +0200

    add time functions - show time

--- server.js
+++ server.js
@@ -15,20 +15,46 @@ io.on("connection", socket => {
 
     socket.on("pseudo", pseudo => {
         socket.pseudo = ent.encode(pseudo);
-        io.emit("new_chatter", {pseudo: socket.pseudo})
-        console.log("pseudo: "+socket.pseudo);
+
+        io.emit("new_chatter", {
+            pseudo: socket.pseudo,
+            time: getTime()
+        })
+        console.log("pseudo: " + socket.pseudo);
     });
 
     socket.on("msg_content", message => {
-        console.log("server receive message: "+message+", by "+socket.pseudo);
-        io.emit("msg_chat", {msg_chat: ent.encode(message), pseudo: socket.pseudo})
+        console.log("server receive message: " + message + ", by " + socket.pseudo);
+
+        io.emit("msg_chat", {
+            msg_chat: ent.encode(message),
+            pseudo: socket.pseudo,
+            time: getTime()
+        })
     })
 
 
     socket.on("disconnect", () => {
-        io.emit("chatter_leave", {pseudo: socket.pseudo})
-        console.log("chatter_leave: "+socket.pseudo)
+
+        io.emit("chatter_leave", {
+            pseudo: socket.pseudo,
+            time: getTime()
+        })
+        console.log("chatter_leave: " + socket.pseudo)
     });
 });
 
 
+/**
+ * 
+ * Functions
+ **/
+
+function getTime() {
+    let date = new Date();
+    let h = (date.getHours() < 10 ? '0' : '') + date.getHours();
+    let m = (date.getMinutes() < 10 ? '0' : '') + date.getMinutes();
+    let s = (date.getSeconds() < 10 ? '0' : '') + date.getSeconds();
+
+    return h + ':' + m + ':' + s;
+}
